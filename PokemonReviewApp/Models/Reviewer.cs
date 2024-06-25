7a087f3fa1207e9038142afa37374df466b7c4b0
rolandexplore93 <roland2rule@gmail.com>
2024-06-25 08:06:40 +0100

    Pokemon review app setup

---
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+    }
+}
