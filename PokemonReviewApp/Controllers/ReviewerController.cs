5cc2382ab22ca30fb5dd9827e4789a345bb4f009
rolandexplore93 <roland2rule@gmail.com>
2024-07-02 11:07:54 +0100

    Implementing api for reviewer endpoints

---
+++ PokemonReviewApp/Controllers/ReviewerController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewerController : Controller
+    {
+        private readonly IReviewerRepository _reviewerRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewerController(IReviewerRepository reviewerRepository, IMapper mapper)
+        {
+            _reviewerRepository = reviewerRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet()]
+        [ProducesResponseType(200)]
+
+    }
+}
--- PokemonReviewApp/Models/Reviewer.cs
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -6,7 +6,5 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public ICollection<Review> Reviews { get; set; }
-
-
     }
 }
